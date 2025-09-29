478d14417e2a2618de862e3bafa06ac58ee71cfc
Vera17-otp <vera24si@mahasiswa.pcr.ac.id>
2025-09-29 10:00:54 +0700

    vera

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\QuestionController;
 
 
 Route::get('/', function () {
@@ -27,4 +28,6 @@ Route::get('/about', function () {
     return view('halaman-about');
 });
 Route::get('/home',[HomeController::class,'index']);
+Route::post('question/store', [QuestionController::class, 'store'])
+		->name('question.store');
 
